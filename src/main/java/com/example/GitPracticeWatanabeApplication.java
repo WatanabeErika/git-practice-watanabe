60d7cf22fe3cceff5a7d644a1e10cc4b9e0f32d2
watanabe erika <erika.watanabe@rakus-partners.co.jp>
2022-05-10 17:38:51 +0900

    ああああ

---
+++ src/main/java/com/example/GitPracticeWatanabeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticeWatanabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticeWatanabeApplication.class, args);
+	}
+
+}
